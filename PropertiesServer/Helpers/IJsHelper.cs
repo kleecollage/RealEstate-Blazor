a7a22aee5eae165adec90f991870a74bb02cf4ab
Klee Collage <klee.collage@gmail.com>
2024-12-23 16:56:45 -0600

    feat(js): Add BlazorJS page with toastr and swal notifications via CDN

---
+++ PropertiesServer/Helpers/IJsHelper.cs
@@ -0,0 +1,16 @@
+using Microsoft.JSInterop;
+
+namespace PropertiesServer.Helpers;
+
+public static class IJsHelper
+{
+    public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+    }
+    
+    public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+    }
+}
